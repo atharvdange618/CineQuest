ae0865d0e936d1a912476e47e890f975c143abd2
Atharv Dange <atharvdange618@gmail.com>
2024-06-01 12:11:21 +0530

    series page completed

--- cinequest/src/Pages/Series/Series.jsx
+++ cinequest/src/Pages/Series/Series.jsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
+import CustomPagination from '../../Components/Pagination/CustomPagination';
+import SingleContent from '../../Components/SingleContent/SingleContent';
+import Genres from '../../Components/Genres/Genres';
+import useGenre from '../../hooks/useGenre';
 
 const Series = () => {
+    const [page, setPage] = useState(1);
+    const [genres, setGenres] = useState([]);
+    const [selectedGenres, setSelectedGenres] = useState([]);
+    const [content, setContent] = useState([]);
+    const [numOfPages, setNumOfPages] = useState();
+    const genreforURL = useGenre(selectedGenres);
+
+    const fetchMovies = async () => {
+        const { data } = await axios.get(
+            `https://api.themoviedb.org/3/discover/tv?api_key=67e3ccc54bfa63bf2f38ece43af5fe80&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_genres=${genreforURL}`
+        );
+        setContent(data.results)
+        setNumOfPages(data.total_pages)
+        console.log(data)
+    };
+
+    useEffect(() => {
+        window.scroll(0, 0);
+        fetchMovies();
+    }, [page, genreforURL]);
+
     return (
-        <span className='pageTitle'>Series</span>
+        <div>
+            <span className='pageTitle'>Series</span>
+            <Genres type="tv" selectedGenres={selectedGenres} setSelectedGenres={setSelectedGenres} genres={genres} setGenres={setGenres} setPage={setPage} />
+            <div className='trending'>
+                {
+                    content && content.map((ele) => <SingleContent key={ele.id} id={ele.id} poster={ele.poster_path} title={ele.title || ele.name} date={ele.first_air_date || ele.release_date} media_type='tv' vote_average={ele.vote_average} />)
+                }
+            </div>
+            {
+                numOfPages > 1 && (<CustomPagination setPage={setPage} numOfPages={numOfPages} />)
+            }
+        </div>
     )
 }
 
